37ec492138846a958ea03cf36f32046d7d928011
furuyatakumi <furuya9706@yahoo.co.jp>
2019-11-08 13:50:11 +0900

    toolbarのレイアウトをXperia XZ1に合わせてレイアウト調整 MainActivity の参照先をToolbarにした。 一応ToolbarActivity.ktも作っといた

--- app/src/main/java/com/example/android/sample/testapp/ToolbarActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/ToolbarActivity.kt
@@ -3,7 +3,7 @@ package com.example.android.sample.testapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ToolbarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
