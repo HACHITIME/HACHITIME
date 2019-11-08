35bdcb3a10767e56a4b0acd44ebcbdf73c86dccc
YSN0000 <etoriya@yahoo.co.jp>
2019-11-08 13:43:23 +0900

    Mappのファイルを作り直し

--- app/src/main/java/com/example/android/sample/testapp/MapActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/MapActivity.kt
@@ -36,7 +36,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        //val sydney = LatLng(-34.0, 151.0)
+        val sydney = LatLng(35.625760, 139.341594)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
