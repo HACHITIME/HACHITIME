2f2853134ad189f269ba3f93b687a13efc3873fe
furuyatakumi <furuya9706@yahoo.co.jp>
2019-11-12 12:02:48 +0900

    ツールバーの遷移（仮）を作成 各ktファイル

--- app/src/main/java/com/example/android/sample/testapp/FacilityListActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/FacilityListActivity.kt
@@ -1,12 +1,47 @@
 package com.example.android.sample.testapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar.*
 
 class FacilityListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_facility_list)
+        setContentView(R.layout.toolbar)
+
+        school_btn.setOnClickListener {
+            val intent = Intent(this, FacilityListActivity::class.java)
+            startActivity(intent)
+        }
+
+        map_btn.setOnClickListener {
+            val intent = Intent(this, FacilityListActivity::class.java)
+            startActivity(intent)
+        }
+
+        stamp_btn.setOnClickListener {
+            val intent = Intent(this, StampActivity::class.java)
+            startActivity(intent)
+        }
+
+        info_btn.setOnClickListener {
+            val intent = Intent(this, InfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        bus_btn.setOnClickListener {
+            val intent = Intent(this, FacilityListActivity::class.java)
+            startActivity(intent)
+        }
+
+        modol_btn.setOnClickListener {
+            val intent = Intent(this, FacilityListActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/android/sample/testapp/MainActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/MainActivity.kt
@@ -2,11 +2,52 @@ package com.example.android.sample.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.KeyEvent
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.toolbar)
+
+        school_btn.setOnClickListener {
+            val intent = Intent(this, FacilityListActivity::class.java)
+            startActivity(intent)
+        }
+
+        map_btn.setOnClickListener {
+            val intent = Intent(this, FacilityListActivity::class.java)
+            startActivity(intent)
+        }
+
+        stamp_btn.setOnClickListener {
+            val intent = Intent(this, StampActivity::class.java)
+            startActivity(intent)
+        }
+
+        info_btn.setOnClickListener {
+            val intent = Intent(this, InfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        bus_btn.setOnClickListener {
+            val intent = Intent(this, FacilityListActivity::class.java)
+            startActivity(intent)
+        }
+
+        modol_btn.setOnClickListener {
+            val intent = Intent(this, FacilityListActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
     }
+
 }
