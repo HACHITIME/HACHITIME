1b63b16827a6adb816460bca328a22917ebf5c0f
HappyDaysKatata <ktt.tmys@gmail.com>
2019-11-15 01:32:03 +0900

    施設一覧を表示するためのViewを作成
    ・GridViewの作成
    ・起動時のファイルをFacilityListActivityへ変更(後にmainActivityへ戻す)
    追加ファイル
    java
     FacilityListAdapter.kt
    drawable
     facility_list_layout.xml
     facilitylist_bijyutukan.jpg
    layout
     rowdata_facility_list.xml

--- app/src/main/java/com/example/android/sample/testapp/FacilityListActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/FacilityListActivity.kt
@@ -2,11 +2,21 @@ package com.example.android.sample.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridView
+
+
 
 class FacilityListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_facility_list)
+        // GridView「facilityList」のIDを取得
+        val list = findViewById<GridView>(R.id.facilityList)
+        // adapterを作成
+        val adapter = FacilityListAdapter(this)
+        // facilityListにadapterをセット
+        list.adapter = adapter
+
     }
 }
---
+++ app/src/main/java/com/example/android/sample/testapp/FacilityListAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.android.sample.testapp
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.LayoutInflater
+
+
+class FacilityListAdapter(
+    private val context: Context
+) : BaseAdapter() {
+
+    val facilityName = arrayOf("研究棟Ｂ","片柳記念ホール","厚生棟","図書館")
+    val facilityImage = arrayOf("facilityList_bijyutukan","facilityList_bijyutukan","facilityList_bijyutukan","facilityList_bijyutukan")
+
+    private class ViewHolder(view: View) {
+        val facilityName = view.findViewById<TextView>(R.id.facilityName)
+        val facilityImage = view.findViewById<ImageView>(R.id.facilityImage)
+    }
+
+    // 与えられた情報からViewを作成し返す
+    private fun createView(parent: ViewGroup?) : View {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.rowdata_facility_list, parent, false)
+        view.tag = ViewHolder(view)
+
+        return view
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        // ビューの作成
+        val view = convertView ?: createView(parent)
+        val viewHolder = view.tag as ViewHolder
+        // 施設名と施設画像を配置
+        viewHolder.facilityName.text = facilityName[position]
+        viewHolder.facilityImage.setImageResource(R.drawable.facilitylist_bijyutukan)
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return facilityName[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return facilityName.size
+    }
+
+}
