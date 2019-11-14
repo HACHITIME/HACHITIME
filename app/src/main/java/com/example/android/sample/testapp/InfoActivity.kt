5781e75c1a3344803cab13f0da66df397fb6ff77
furuyatakumi <furuya9706@yahoo.co.jp>
2019-11-14 14:09:44 +0900

    遷移のActionを変更（全部）

--- app/src/main/java/com/example/android/sample/testapp/InfoActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/InfoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.sample.testapp
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -16,27 +17,32 @@ class InfoActivity : AppCompatActivity(){
 
         school_btn.setOnClickListener {
             val intent = Intent(this, FacilityListActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         map_btn.setOnClickListener {
             val intent = Intent(this, MapActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         stamp_btn.setOnClickListener {
             val intent = Intent(this, StampActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         info_btn.setOnClickListener {
             val intent = Intent(this, InfoActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         bus_btn.setOnClickListener {
             val intent = Intent(this, BustimeActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         modol_btn.setOnClickListener {
--- app/src/main/java/com/example/android/sample/testapp/MapActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/MapActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.sample.testapp
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -15,27 +16,32 @@ class MapActivity : AppCompatActivity() {
 
         school_btn.setOnClickListener {
             val intent = Intent(this, FacilityListActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         map_btn.setOnClickListener {
             val intent = Intent(this, MapActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         stamp_btn.setOnClickListener {
             val intent = Intent(this, StampActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         info_btn.setOnClickListener {
             val intent = Intent(this, InfoActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         bus_btn.setOnClickListener {
             val intent = Intent(this, BustimeActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
 
         modol_btn.setOnClickListener {
