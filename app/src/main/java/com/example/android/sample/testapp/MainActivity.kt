dab61fcda1917d6ae168f852784e13da9554280a
furuyatakumi <furuya9706@yahoo.co.jp>
2019-12-03 11:01:28 +0900

    スタンプ画面

--- app/src/main/java/com/example/android/sample/testapp/MainActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/MainActivity.kt
@@ -2,13 +2,15 @@ package com.example.android.sample.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_stamp.*
 
 
 open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.map_hopup)
+        setContentView(R.layout.activity_stamp)
+
 
     }
 
