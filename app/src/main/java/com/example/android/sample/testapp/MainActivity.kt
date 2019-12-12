c5b400dbc0b24ac2b1ebe9728033d08ca8d436a2
furuyatakumi <furuya9706@yahoo.co.jp>
2019-12-12 11:24:03 +0900

    ホップアップ作成途中

--- app/src/main/java/com/example/android/sample/testapp/MainActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/MainActivity.kt
@@ -2,28 +2,17 @@ package com.example.android.sample.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_stamp.*
 
 
 open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_stamp)
-
-        imageView7.setOnClickListener {
-            // スタンプ詳細ダイアログをを表示
-            val dialog = HopupDialog()
-            // スタンプ詳細を渡す
-            val args = Bundle()
-            dialog.setArguments(args)
-            dialog.show(supportFragmentManager, "simple")
+        setContentView(R.layout.activity_main)
 
         }
 
 
 
-
-    }
 
 }
--- app/src/main/java/com/example/android/sample/testapp/StampActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/StampActivity.kt
@@ -1,51 +1,29 @@
 package com.example.android.sample.testapp
 
-import android.app.ActivityOptions
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.toolbar.*
+import kotlinx.android.synthetic.main.activity_stamp.*
 
-class StampActivity : AppCompatActivity() {
+
+open class StampActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stamp)
-        setContentView(R.layout.toolbar)
-        tytle.text = "Stamp"
 
-        school_btn.setOnClickListener {
-            val intent = Intent(this, FacilityListActivity::class.java)
-            startActivity(intent,
-                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        imageView7.setOnClickListener {
+            // スタンプ詳細ダイアログをを表示
+            val dialog = HopupDialog()
+            // スタンプ詳細を渡す
+            val args = Bundle()
+            dialog.setArguments(args)
+            dialog.show(supportFragmentManager, "simple")
+
         }
 
-        map_btn.setOnClickListener {
-            val intent = Intent(this, MapActivity::class.java)
-            startActivity(intent,
-                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
-        }
 
-        stamp_btn.setOnClickListener {
-            val intent = Intent(this, StampActivity::class.java)
-            startActivity(intent,
-                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
-        }
 
-        info_btn.setOnClickListener {
-            val intent = Intent(this, InfoActivity::class.java)
-            startActivity(intent,
-                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
-        }
 
-        bus_btn.setOnClickListener {
-            val intent = Intent(this, BustimeActivity::class.java)
-            startActivity(intent,
-                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
-        }
-
-        modol_btn.setOnClickListener {
-            super.onBackPressed()
-        }
     }
+
 }
