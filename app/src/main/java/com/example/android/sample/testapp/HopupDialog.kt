66d23ffedb914b873cba26b60693ec050fd48326
furuyatakumi <furuya9706@yahoo.co.jp>
2019-12-18 12:04:24 +0900

    スタンプ専用のホップアップを制作 ホップアップを一応全部のボタンに適用

--- app/src/main/java/com/example/android/sample/testapp/HopupDialog.kt
+++ app/src/main/java/com/example/android/sample/testapp/HopupDialog.kt
@@ -3,6 +3,8 @@ package com.example.android.sample.testapp
 import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
+import kotlinx.android.synthetic.main.activity_stamp.*
+import kotlinx.android.synthetic.main.stamp_hopup.*
 
 open class HopupDialog : DialogFragment() {
 
@@ -10,8 +12,12 @@ open class HopupDialog : DialogFragment() {
 
         // ダイアログの作成
         val dialog = Dialog(getActivity())
-        dialog.setContentView(R.layout.map_hopup)
+        dialog.setContentView(R.layout.stamp_hopup)
 
         return dialog
+
+        close_btn.setOnClickListener {
+
+            }
+        }
     }
-}
--- app/src/main/java/com/example/android/sample/testapp/StampActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/StampActivity.kt
@@ -7,9 +7,70 @@ import kotlinx.android.synthetic.main.activity_stamp.*
 
 open class StampActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stamp)
+
+        imageView.setOnClickListener {
+            // スタンプ詳細ダイアログをを表示
+            val dialog = HopupDialog()
+            // スタンプ詳細を渡す
+            val args = Bundle(0)
+            dialog.setArguments(args)
+            dialog.show(supportFragmentManager, "simple")
+
+        }
+
+        imageView2.setOnClickListener {
+            // スタンプ詳細ダイアログをを表示
+            val dialog = HopupDialog()
+            // スタンプ詳細を渡す
+            val args = Bundle()
+            dialog.setArguments(args)
+            dialog.show(supportFragmentManager, "simple")
+
+        }
+
+        imageView3.setOnClickListener {
+            // スタンプ詳細ダイアログをを表示
+            val dialog = HopupDialog()
+            // スタンプ詳細を渡す
+            val args = Bundle()
+            dialog.setArguments(args)
+            dialog.show(supportFragmentManager, "simple")
+
+        }
+
+        imageView4.setOnClickListener {
+            // スタンプ詳細ダイアログをを表示
+            val dialog = HopupDialog()
+            // スタンプ詳細を渡す
+            val args = Bundle()
+            dialog.setArguments(args)
+            dialog.show(supportFragmentManager, "simple")
+
+        }
+
+        imageView5.setOnClickListener {
+            // スタンプ詳細ダイアログをを表示
+            val dialog = HopupDialog()
+            // スタンプ詳細を渡す
+            val args = Bundle()
+            dialog.setArguments(args)
+            dialog.show(supportFragmentManager, "simple")
+
+        }
+
+        imageView6.setOnClickListener {
+            // スタンプ詳細ダイアログをを表示
+            val dialog = HopupDialog()
+            // スタンプ詳細を渡す
+            val args = Bundle()
+            dialog.setArguments(args)
+            dialog.show(supportFragmentManager, "simple")
+
+        }
 
         imageView7.setOnClickListener {
             // スタンプ詳細ダイアログをを表示
@@ -20,6 +81,8 @@ open class StampActivity : AppCompatActivity() {
             dialog.show(supportFragmentManager, "simple")
 
         }
+
+
 
 
 
