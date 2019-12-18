2ce3f8f3c019b05241f9ae6848e3e6a67bc8c0d3
furuyatakumi <furuya9706@yahoo.co.jp>
2019-12-18 12:20:40 +0900

    閉じるボタン実装

--- app/src/main/java/com/example/android/sample/testapp/HopupDialog.kt
+++ app/src/main/java/com/example/android/sample/testapp/HopupDialog.kt
@@ -3,6 +3,8 @@ package com.example.android.sample.testapp
 import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
+import android.view.View
+import android.widget.Button
 
 
 open class HopupDialog : DialogFragment() {
@@ -12,6 +14,12 @@ open class HopupDialog : DialogFragment() {
         // ダイアログの作成
         val dialog = Dialog(getActivity())
         dialog.setContentView(R.layout.stamp_hopup)
+
+        // 「X」ボタン投下時の処理
+        dialog.findViewById<Button>(R.id.close_btn).setOnClickListener(View.OnClickListener {
+            // ダイアログを閉じる
+            dismiss()
+        })
 
         return dialog
     }
