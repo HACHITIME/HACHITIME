edad69b97191ee748646591c72728bc3fb5dd58e
furuyatakumi <furuya9706@yahoo.co.jp>
2019-12-19 15:59:10 +0900

    配列完成 画像を各スタンプに適応

--- app/src/main/java/com/example/android/sample/testapp/HopupDialog.kt
+++ app/src/main/java/com/example/android/sample/testapp/HopupDialog.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_stamp.*
 
 
 open class HopupDialog : DialogFragment() {
@@ -20,6 +21,7 @@ open class HopupDialog : DialogFragment() {
         dialog.findViewById<TextView>(R.id.Facility_Name).setText(arguments!!.getString("stamp_name"))
         //画像取得
         dialog.findViewById<ImageView>(R.id.ImageViewArea).setImageResource(arguments!!.getInt("stamp_image"))
+
 
         // 「X」ボタン投下時の処理
         dialog.findViewById<Button>(R.id.close_btn).setOnClickListener(View.OnClickListener {
--- app/src/main/java/com/example/android/sample/testapp/StampActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/StampActivity.kt
@@ -2,28 +2,35 @@ package com.example.android.sample.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_stamp.*
 
 
 open class StampActivity : AppCompatActivity() {
 
-    val stampName = arrayListOf<String>("展示棟","図書館棟","講義棟A","体育館","メディアホール","片柳記念ホール","片柳研究棟")
-    val stampImage= arrayListOf<String>()
+    val stampName = arrayListOf("展示棟","図書館棟","講義棟A","体育館","メディアホール","片柳記念ホール","片柳研究棟")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stamp)
 
+
+        //スタンプ７個を押した時のアクション
         imageView.setOnClickListener {
             // スタンプ詳細ダイアログをを表示
             val dialog = HopupDialog()
+            // 画像をidに変換
+            val imgId = resources.getIdentifier(
+                "tecnoroji",
+                "drawable",
+                packageName
+            )
+
             // スタンプ詳細を渡す
             val args = Bundle(0)
             dialog.setArguments(args)
             args.putString("stamp_name", stampName[0])
-            args.putString("stamp_image", stampImage[0])
+            args.putInt("stamp_image", imgId)
             dialog.show(supportFragmentManager, "simple")
 
         }
@@ -31,11 +38,17 @@ open class StampActivity : AppCompatActivity() {
         imageView2.setOnClickListener {
             // スタンプ詳細ダイアログをを表示
             val dialog = HopupDialog()
+            // 画像をidに変換
+            val imgId = resources.getIdentifier(
+                "curieita",
+                "drawable",
+                packageName
+            )
             // スタンプ詳細を渡す
             val args = Bundle()
             dialog.setArguments(args)
             args.putString("stamp_name", stampName[1])
-            args.putString("stamp_image", stampImage[1])
+            args.putInt("stamp_image", imgId)
             dialog.show(supportFragmentManager, "simple")
 
         }
@@ -43,11 +56,17 @@ open class StampActivity : AppCompatActivity() {
         imageView3.setOnClickListener {
             // スタンプ詳細ダイアログをを表示
             val dialog = HopupDialog()
+            // 画像をidに変換
+            val imgId = resources.getIdentifier(
+                "it",
+                "drawable",
+                packageName
+            )
             // スタンプ詳細を渡す
             val args = Bundle()
             dialog.setArguments(args)
             args.putString("stamp_name", stampName[2])
-            args.putString("stamp_image", stampImage[2])
+            args.putInt("stamp_image", imgId)
             dialog.show(supportFragmentManager, "simple")
 
         }
@@ -55,11 +74,17 @@ open class StampActivity : AppCompatActivity() {
         imageView4.setOnClickListener {
             // スタンプ詳細ダイアログをを表示
             val dialog = HopupDialog()
+            // 画像をidに変換
+            val imgId = resources.getIdentifier(
+                "sports",
+                "drawable",
+                packageName
+            )
             // スタンプ詳細を渡す
             val args = Bundle()
             dialog.setArguments(args)
             args.putString("stamp_name", stampName[3])
-            args.putString("stamp_image", stampImage[3])
+            args.putInt("stamp_image", imgId)
             dialog.show(supportFragmentManager, "simple")
 
         }
@@ -67,11 +92,17 @@ open class StampActivity : AppCompatActivity() {
         imageView5.setOnClickListener {
             // スタンプ詳細ダイアログをを表示
             val dialog = HopupDialog()
+            // 画像をidに変換
+            val imgId = resources.getIdentifier(
+                "music",
+                "drawable",
+                packageName
+            )
             // スタンプ詳細を渡す
             val args = Bundle()
             dialog.setArguments(args)
             args.putString("stamp_name", stampName[4])
-            args.putString("stamp_image", stampImage[4])
+            args.putInt("stamp_image", imgId)
             dialog.show(supportFragmentManager, "simple")
 
         }
@@ -79,11 +110,17 @@ open class StampActivity : AppCompatActivity() {
         imageView6.setOnClickListener {
             // スタンプ詳細ダイアログをを表示
             val dialog = HopupDialog()
+            // 画像をidに変換
+            val imgId = resources.getIdentifier(
+                "iryou",
+                "drawable",
+                packageName
+            )
             // スタンプ詳細を渡す
             val args = Bundle()
             dialog.setArguments(args)
             args.putString("stamp_name", stampName[5])
-            args.putString("stamp_image", stampImage[5])
+            args.putInt("stamp_image", imgId)
             dialog.show(supportFragmentManager, "simple")
 
         }
@@ -91,10 +128,16 @@ open class StampActivity : AppCompatActivity() {
         imageView7.setOnClickListener {
             // スタンプ詳細ダイアログをを表示
             val dialog = HopupDialog()
+            // 画像をidに変換
+            val imgId = resources.getIdentifier(
+                "desain",
+                "drawable",
+                packageName
+            )
             // スタンプ詳細を渡す
             val args = Bundle()
             args.putString("stamp_name", stampName[6])
-            args.putString("stamp_image", stampImage[6])
+            args.putInt("stamp_image", imgId)
             dialog.setArguments(args)
             dialog.show(supportFragmentManager, "simple")
 
