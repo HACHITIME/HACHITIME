57db6f24a8cc46b0f29a08389859a0aa18f639b2
G016C1104 <tanasa759@yahoo.co.jp>
2019-12-20 10:55:40 +0900

    da

--- app/src/main/java/com/example/android/sample/testapp/HopupDialog.kt
+++ app/src/main/java/com/example/android/sample/testapp/HopupDialog.kt
@@ -1,5 +1,5 @@
-package com.example.android.sample.testapp
 
+package com.example.android.sample.testapp
 import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
@@ -8,12 +8,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_stamp.*
-
-
 open class HopupDialog : DialogFragment() {
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-
         // ダイアログの作成
         val dialog = Dialog(getActivity())
         dialog.setContentView(R.layout.stamp_hopup)
@@ -21,15 +17,11 @@ open class HopupDialog : DialogFragment() {
         dialog.findViewById<TextView>(R.id.Facility_Name).setText(arguments!!.getString("stamp_name"))
         //画像取得
         dialog.findViewById<ImageView>(R.id.ImageViewArea).setImageResource(arguments!!.getInt("stamp_image"))
-
-
         // 「X」ボタン投下時の処理
         dialog.findViewById<Button>(R.id.close_btn).setOnClickListener(View.OnClickListener {
             // ダイアログを閉じる
             dismiss()
         })
-
         return dialog
     }
-
 }
