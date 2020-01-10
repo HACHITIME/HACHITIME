7c7d2072e7b5c369da8a76a7761dd132778b185f
furuyatakumi <furuya9706@yahoo.co.jp>
2020-01-10 13:17:40 +0900

    学内情報画面β仮完成

--- app/src/main/java/com/example/android/sample/testapp/MapActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/MapActivity.kt
@@ -1,14 +1,17 @@
 package com.example.android.sample.testapp
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MapActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.campasinfo)
+        //setContentView(R.layout.toolbar)
 
     }
 
