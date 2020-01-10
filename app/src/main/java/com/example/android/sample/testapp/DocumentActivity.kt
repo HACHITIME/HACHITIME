185e745c2d42a8b2839194c2bae141f2313eb6cc
furuyatakumi <furuya9706@yahoo.co.jp>
2020-01-10 10:00:24 +0900

    ツールバー追加(仮完成版)

--- app/src/main/java/com/example/android/sample/testapp/DocumentActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/DocumentActivity.kt
@@ -1,10 +1,18 @@
 package com.example.android.sample.testapp
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_calender.*
+import kotlinx.android.synthetic.main.activity_calender.bus_btn
+import kotlinx.android.synthetic.main.activity_calender.info_btn
+import kotlinx.android.synthetic.main.activity_calender.map_btn
+import kotlinx.android.synthetic.main.activity_calender.modol_btn
+import kotlinx.android.synthetic.main.activity_calender.stamp_btn
+import kotlinx.android.synthetic.main.activity_campusinfo.*
 
 class DocumentActivity : AppCompatActivity() {
 
@@ -14,6 +22,43 @@ class DocumentActivity : AppCompatActivity() {
 
         webview.setWebViewClient(WebViewClient())
         webview.loadUrl("https://www.neec.ac.jp/cgi-bin/requests.php?type=0")
+
+        //ツールバー
+        map_btn.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            // 同一アクティビティ開始時、古い方を終了させる
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        stamp_btn.setOnClickListener {
+            val intent = Intent(this, StampActivity::class.java)
+            // 同一アクティビティ開始時、古い方を終了させる
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        info_btn.setOnClickListener {
+            val intent = Intent(this, CampusInfoActivity::class.java)
+            // 同一アクティビティ開始時、古い方を終了させる
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        bus_btn.setOnClickListener {
+            val intent = Intent(this, BustimeActivity::class.java)
+            // 同一アクティビティ開始時、古い方を終了させる
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        modol_btn.setOnClickListener {
+            super.onBackPressed()
+        }
 
 
     }
--- app/src/main/java/com/example/android/sample/testapp/SubjectActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/SubjectActivity.kt
@@ -1,10 +1,18 @@
 package com.example.android.sample.testapp
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_calender.*
+import kotlinx.android.synthetic.main.activity_calender.bus_btn
+import kotlinx.android.synthetic.main.activity_calender.info_btn
+import kotlinx.android.synthetic.main.activity_calender.map_btn
+import kotlinx.android.synthetic.main.activity_calender.modol_btn
+import kotlinx.android.synthetic.main.activity_calender.stamp_btn
+import kotlinx.android.synthetic.main.activity_campusinfo.*
 
 class SubjectActivity : AppCompatActivity() {
 
@@ -14,6 +22,43 @@ class SubjectActivity : AppCompatActivity() {
 
         webview.setWebViewClient(WebViewClient())
         webview.loadUrl("https://www.neec.ac.jp/department/")
+
+        //ツールバー
+        map_btn.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            // 同一アクティビティ開始時、古い方を終了させる
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        stamp_btn.setOnClickListener {
+            val intent = Intent(this, StampActivity::class.java)
+            // 同一アクティビティ開始時、古い方を終了させる
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        info_btn.setOnClickListener {
+            val intent = Intent(this, CampusInfoActivity::class.java)
+            // 同一アクティビティ開始時、古い方を終了させる
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        bus_btn.setOnClickListener {
+            val intent = Intent(this, BustimeActivity::class.java)
+            // 同一アクティビティ開始時、古い方を終了させる
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        modol_btn.setOnClickListener {
+            super.onBackPressed()
+        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -26,4 +71,6 @@ class SubjectActivity : AppCompatActivity() {
 
         return super.onKeyDown(keyCode, event)
     }
+
+
 }
