64a2dcf4e1050d102914fede39c064a73aed3065
YSN0000 <etoriya@yahoo.co.jp>
2020-01-21 09:57:22 +0900

    判定できた

--- app/src/main/java/com/example/android/sample/testapp/MapActivityDialog.kt
+++ app/src/main/java/com/example/android/sample/testapp/MapActivityDialog.kt
@@ -1,11 +1,8 @@
 package com.example.android.sample.testapp
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
