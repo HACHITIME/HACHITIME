4e3506de8550b424d018b8e0e900497ac3fd0689
YSN0000 <etoriya@yahoo.co.jp>
2020-02-19 11:03:32 +0900

    マーカー変更

--- app/src/main/java/com/example/android/sample/testapp/MapActivity.kt
+++ app/src/main/java/com/example/android/sample/testapp/MapActivity.kt
@@ -210,7 +210,8 @@ class MapActivity : AppCompatActivity(),OnMapReadyCallback,LocationListener{
         mMap.moveCamera(update)
 
         val kenkyutoA = LatLng(35.62535, 139.342350000005)
-        mMap.addMarker(MarkerOptions().position(kenkyutoA).title("研究棟A").infoWindowAnchor(30F,30F).icon(BitmapDescriptorFactory.fromResource(R.drawable.googlemap_kenkyutoA.png)))
+        mMap.addMarker(MarkerOptions().position(kenkyutoA).title("研究棟A")
+            .infoWindowAnchor(30F,30F).icon(BitmapDescriptorFactory.fromResource(R.drawable.googlemap_kenkyuto_a)))
 
         val kenkyutoB = LatLng(35.625, 139.34180090000005)
         mMap.addMarker(MarkerOptions().position(kenkyutoB).title("研究棟B").infoWindowAnchor(30F,30F))
